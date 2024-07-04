d98bf1579e7e4346c4f968db40af4f777b12c8bc
ganari3518 <h3064l@gmail.com>
2024-07-04 19:28:44 +0900

    feat : update CORS Config

--- src/main/java/com/syu/capstone_stock/controller/StockController.java
+++ src/main/java/com/syu/capstone_stock/controller/StockController.java
@@ -1,5 +1,7 @@
 package com.syu.capstone_stock.controller;
 
+import com.syu.capstone_stock.util.ClientUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,20 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", maxAge = 3000, allowCredentials = "true")
 public class StockController {
     @GetMapping("/")
-    public String openIndex() {
+    public String openIndex(HttpServletRequest request) {
+        ClientUtils.getRemoteIP(request);
         return "index";
     }
 
     @GetMapping("/stock")
-    public String openInfo(){
+    public String openInfo(HttpServletRequest request){
+        ClientUtils.getRemoteIP(request);
         return "/stock_info";
     }
 
     @GetMapping("/search")
-    public String search(){
+    public String search(HttpServletRequest request){
+        ClientUtils.getRemoteIP(request);
         return "/search";
     }
 
