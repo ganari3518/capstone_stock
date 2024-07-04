40f8d1f9227ecaa1b017e131e52d17e080358ec2
ganari3518 <h3064l@gmail.com>
2024-07-04 19:22:38 +0900

    feat : add Schedule Config

--- src/main/java/com/syu/capstone_stock/CapstoneStockApplication.java
+++ src/main/java/com/syu/capstone_stock/CapstoneStockApplication.java
@@ -2,11 +2,12 @@ package com.syu.capstone_stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CapstoneStockApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapstoneStockApplication.class, args);
     }
-
 }
