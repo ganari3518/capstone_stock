462f8f930dad412a818b65829d0ae7070f5b29a2
ganari3518 <h3064l@gmail.com>
2024-07-04 19:26:52 +0900

    feat : add CORS Config

--- src/main/java/com/syu/capstone_stock/controller/PostController.java
+++ src/main/java/com/syu/capstone_stock/controller/PostController.java
@@ -3,19 +3,24 @@ package com.syu.capstone_stock.controller;
 import com.syu.capstone_stock.domain.Post;
 import com.syu.capstone_stock.dto.PostRequestDto;
 import com.syu.capstone_stock.service.PostService;
+import com.syu.capstone_stock.util.ClientUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", maxAge = 3000, allowCredentials = "true")
 public class PostController {
     private final PostService postService;
     @GetMapping("/key/write.do")
-    public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model){
+    public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model, HttpServletRequest request){
+        ClientUtils.getRemoteIP(request);
         if(id != null){
             Post post = postService.findById(id);
             model.addAttribute("post",post);
@@ -24,25 +29,29 @@ public class PostController {
     }
 
     @PostMapping("/key/save.do")
-    public String savePost(final PostRequestDto params){
+    public String savePost(final PostRequestDto params, HttpServletRequest request){
+        ClientUtils.getRemoteIP(request);
         postService.save(params);
         return "redirect:/setting/list.do";
     }
 
     @PostMapping("/key/update.do")
-    public String updatePost(PostRequestDto params, Model mode){
+    public String updatePost(PostRequestDto params, Model model, HttpServletRequest request){
+        ClientUtils.getRemoteIP(request);
         postService.update(params);
         return "redirect:/setting/list.do";
     }
 
     @PostMapping("/key/delete.do")
-    public String deletePost(@RequestParam final Long id){
+    public String deletePost(@RequestParam final Long id, HttpServletRequest request){
+        ClientUtils.getRemoteIP(request);
         postService.deleteById(id);
         return "redirect:/setting/list.do";
     }
 
     @GetMapping("/key/view.do")
-    public String openPostView(@RequestParam final Long id, Model model){
+    public String openPostView(@RequestParam final Long id, Model model, HttpServletRequest request){
+        ClientUtils.getRemoteIP(request);
         Post post = postService.findById(id);
         model.addAttribute("post",post);
         return "/key/view";
