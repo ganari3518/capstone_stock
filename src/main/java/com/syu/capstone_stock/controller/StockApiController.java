6efc497d3578523952ae8daf4a865c5399a3c16d
ganari3518 <h3064l@gmail.com>
2024-07-04 19:28:31 +0900

    feat : update CORS Config

--- src/main/java/com/syu/capstone_stock/controller/StockApiController.java
+++ src/main/java/com/syu/capstone_stock/controller/StockApiController.java
@@ -1,7 +1,9 @@
 package com.syu.capstone_stock.controller;
 
 import com.syu.capstone_stock.service.PyService;
+import com.syu.capstone_stock.util.ClientUtils;
 import com.syu.capstone_stock.util.FileUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,37 +14,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", maxAge = 3000, allowCredentials = "true")
 public class StockApiController {
     private final PyService pyService;
 
     @GetMapping("/caps/info/top10")
-    public String findStockTop10() {
+    public String findStockTop10(HttpServletRequest request) {
+        ClientUtils.getRemoteIP(request);
         return pyService.findStockTop10();
     }
 
     @GetMapping("/caps/graph/{Code}")
-    public void saveGraph(@PathVariable String Code) {
+    public void saveGraph(@PathVariable String Code, HttpServletRequest request) {
+        ClientUtils.getRemoteIP(request);
         pyService.saveGraph(Code);
     }
 
     @GetMapping("/caps/info/{Code}")
-    public String findStockInfo(@PathVariable String Code) {
+    public String findStockInfo(@PathVariable String Code, HttpServletRequest request) {
+        ClientUtils.getRemoteIP(request);
         return pyService.findStockInfo(Code);
     }
 
     @GetMapping("/caps/info/graph/{Code}/{filename}")
-    public ResponseEntity<?> getGraphImg(@PathVariable String Code, @PathVariable String filename){
+    public ResponseEntity<?> getGraphImg(@PathVariable String Code, @PathVariable String filename, HttpServletRequest request){
+        ClientUtils.getRemoteIP(request);
         return FileUtil.getResource(Code, filename);
     }
 
     @GetMapping("/caps/info/stocks")
-    public String findAllStock() {
+    public String findAllStock(HttpServletRequest request) {
+        ClientUtils.getRemoteIP(request);
         return pyService.findAllStock();
     }
 
     @GetMapping("/caps/info/exchanges")
-    public String findExchange(){
+    public String findExchange(HttpServletRequest request){
+        ClientUtils.getRemoteIP(request);
         return pyService.findExchange();
     }
 
